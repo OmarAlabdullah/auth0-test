1d3b2af2c5c875b264ef30087b290569f194c418
Omar Alabdullah <omar@xel.nl>
2022-12-15 09:26:04 +0100

    fix logout method

--- src/main/java/com/example/auth0test/security/SecurityConfig.java
+++ src/main/java/com/example/auth0test/security/SecurityConfig.java
@@ -4,10 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig {
+    private final LogoutHandler logoutHandler;
 
+    public SecurityConfig(LogoutHandler logoutHandler) {
+        this.logoutHandler = logoutHandler;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -15,6 +21,9 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
                 .and().oauth2Login();
         return http.oauth2Login()
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .addLogoutHandler(logoutHandler)
                 .and().build();
     }
 }
